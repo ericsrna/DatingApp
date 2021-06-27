9bc189573faf190ac79713d811deb4f070a813ac
ericsrna <eric.srna@gmail.com>
2021-06-27 15:24:19 -0400

    End of section 13

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultcontext = await next();
+
+            if (!resultcontext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var userId = resultcontext.HttpContext.User.GetUserId();
+            var repo = resultcontext.HttpContext.RequestServices.GetService<IUserRepository>();
+            var user = await repo.GetUserByIdAsync(userId);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+        }
+    }
+}
